5eddaba0ed0987348accb615eb757fe3bddb147b
Israel Wong <israelwong@iMac-de-Israel.local>
2024-04-15 18:47:23 -0600

    3er commit

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import { defineConfig } from "vite";
 
 export default defineConfig({
   plugins: [react()],
-  // base: "/promedia/",
+  base: "/promedia/",
   // resolve: {
   //   alias: {
   //     "@": path.resolve(__dirname, "./src"),
